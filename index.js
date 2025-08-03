ebb850cf3bba2379ebc0d5f2640ccbc66d2aa4cf
shikhasingh12710 <shikhasingh9138@gmail.com>
2025-08-03 18:44:26 +0530

    github data added

--- index.js
+++ index.js
@@ -3,6 +3,45 @@ const express = require('express')
 const app = express()
 const port = 4000
 
+
+const githubData=
+    {
+  "login": "shikhasingh",
+  "id": 6201536,
+  "node_id": "MDQ6VXNlcjYyMDE1MzY=",
+  "avatar_url": "https://avatars.githubusercontent.com/u/6201536?v=4",
+  "gravatar_id": "",
+  "url": "https://api.github.com/users/shikhasingh",
+  "html_url": "https://github.com/shikhasingh",
+  "followers_url": "https://api.github.com/users/shikhasingh/followers",
+  "following_url": "https://api.github.com/users/shikhasingh/following{/other_user}",
+  "gists_url": "https://api.github.com/users/shikhasingh/gists{/gist_id}",
+  "starred_url": "https://api.github.com/users/shikhasingh/starred{/owner}{/repo}",
+  "subscriptions_url": "https://api.github.com/users/shikhasingh/subscriptions",
+  "organizations_url": "https://api.github.com/users/shikhasingh/orgs",
+  "repos_url": "https://api.github.com/users/shikhasingh/repos",
+  "events_url": "https://api.github.com/users/shikhasingh/events{/privacy}",
+  "received_events_url": "https://api.github.com/users/shikhasingh/received_events",
+  "type": "User",
+  "user_view_type": "public",
+  "site_admin": false,
+  "name": "Shikha Singh",
+  "company": "Williams College",
+  "blog": "",
+  "location": "Williamstown",
+  "email": null,
+  "hireable": null,
+  "bio": null,
+  "twitter_username": null,
+  "public_repos": 3,
+  "public_gists": 0,
+  "followers": 0,
+  "following": 2,
+  "created_at": "2013-12-17T00:25:20Z",
+  "updated_at": "2025-07-25T13:52:35Z"
+}
+
+
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
@@ -14,6 +53,10 @@ app.get('/login',(req,res)=>{
 })
 app.get('/youtube',(req,res)=>{
     res.send('<h2>Chai and code</h2>')
+})
+
+app.get('/github',(req,res)=>{
+    res.json(githubData)
 })
 app.listen(process.env.PORT, () => {
   console.log(`Example app listening on port ${port}`)
